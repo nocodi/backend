5a7fbc5df51c4d2ab1b0d30e3f108025a4703443
shoshtari <mr.malekey@gmail.com>
2025-03-08 06:04:18 +0330

    feat: Add shell plus

--- nocodi/settings.py
+++ nocodi/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "drf_spectacular",
     "iam.apps.IamConfig",
 ]
@@ -85,7 +86,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
 }
 
 
