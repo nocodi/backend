8094dc3e8065c220a5a49277a865ea5d7dc6f793
Ali Kamali <seyedalikamali1381@gmail.com>
2025-03-22 12:42:53 +0330

    updates urls

--- nocodi/urls.py
+++ nocodi/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -41,3 +43,5 @@ urlpatterns = [
     ),
     path("iam/", include(("iam.urls", "iam"), namespace="iam")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
