4f55ef2cbc64bac28a0b16fc8fbfb025b5759077
Ali Kamali <seyedalikamali1381@gmail.com>
2025-03-22 12:48:00 +0330

    updates static urls

--- nocodi/settings.py
+++ nocodi/settings.py
@@ -147,8 +147,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_files")]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
