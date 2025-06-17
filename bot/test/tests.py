d68db18cf3700d9d88ba581f9badb71924520872
Ali Kamali <wwwwwq37@gmail.com>
2025-06-17 19:54:30 +0330

    update test to scenario test

--- bot/test/tests.py
+++ bot/test/tests.py
@@ -1,14 +1,17 @@
-import unittest
-from io import BytesIO
-
-from django.core.files.base import ContentFile
 from django.test import TestCase
 from django.urls import reverse
 from PIL import Image
 from rest_framework import status
 
 from bot.models import Bot
-from component.models import CodeComponent, Component, Markup, OnMessage, SetState
+from component.models import (
+    CodeComponent,
+    Component,
+    Markup,
+    OnMessage,
+    SetState,
+    SwitchComponent,
+)
 from component.telegram.models import SendMessage, SendPhoto
 from iam.models import IamUser
 from iam.utils import create_token_for_iamuser
@@ -35,126 +38,180 @@ class CodeTest(TestCase):
             component_type=Component.ComponentType.TRIGGER,
             text="/start",
         )
-        send_message_comp = SendMessage.objects.create(
+        send_message_start = SendMessage.objects.create(
             bot=self.bot,
             chat_id=".from_user.id",
-            text="Hello, World!",
+            text="Hello welcome to the bot",
             position_x=1,
             position_y=1,
             previous_component=on_message_component,
         )
-        send1 = SendMessage.objects.create(
-            bot=self.bot,
-            chat_id=".from_user.id",
-            text="Bye World",
-            position_x=1,
-            position_y=1,
-            previous_component=on_message_component,
-        )
-
-        send_3 = SendMessage.objects.create(
-            bot=self.bot,
-            chat_id=".from_user.id",
-            text="Button clicked!",
-            position_x=1,
-            position_y=1,
-        )
-        send_4 = SendMessage.objects.create(
-            bot=self.bot,
-            chat_id=".from_user.id",
-            text="Button clicked 2",
-            position_x=1,
-            position_y=1,
-            previous_component=send_3,
-        )
-
-        Markup.objects.create(
-            parent_component=send1,
-            markup_type=Markup.MarkupType.InlineKeyboard,
-            buttons=[
-                [{"value": "Button 1"}, {"value": "Button 2"}],
-                [{"value": "Button 3"}, {"value": "Button 4"}],
-                [{"value": "Button 5", "next_component": send_3.id}],
-            ],
-        )
-
-        image = Image.new("RGB", (100, 100), color="blue")
-        image_io = BytesIO()
-        image.save(image_io, "PNG")
-        image_content = ContentFile(image_io.getvalue(), "test.png")
-        # photo_component = SendPhoto.objects.create(
-        #     bot=self.bot,
-        #     chat_id=".from_user.id",
-        #     photo=image_content,
-        #     position_x=1,
-        #     position_y=1,
-        #     previous_component=on_message_component,
-        # )
-
-        SetState.objects.create(
-            bot=self.bot,
-            state="state",
-            position_x=1,
-            position_y=1,
-            previous_component=on_message_component,
-        )
-
-        on_state_component = OnMessage.objects.create(
-            bot=self.bot,
-            state="state",
-            position_x=1,
-            position_y=1,
-        )
-
-        send2 = SendMessage.objects.create(
-            bot=self.bot,
-            chat_id=".from_user.id",
-            text="State set",
-            position_x=1,
-            position_y=1,
-            previous_component=on_state_component,
-        )
-
         CodeComponent.objects.create(
             bot=self.bot,
-            code="print('Hello, World!')",
+            code="print('This is log message to validate code')",
             position_x=1,
             position_y=1,
-            previous_component=send2,
+            previous_component=on_message_component,
         )
 
-        send_5 = SendMessage.objects.create(
+        send_support = SendMessage.objects.create(
             bot=self.bot,
             chat_id=".from_user.id",
-            text="Button 3 clicked from text button",
+            text="Send your message then i want to send to admin",
+            position_x=1,
+            position_y=1,
+        )
+        SetState.objects.create(
+            bot=self.bot,
+            state="support",
+            position_x=1,
+            position_y=1,
+            previous_component=send_support,
+        )
+
+        send_message_help = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id=".from_user.id",
+            text="This is test help message",
+            position_x=1,
+            position_y=1,
+        )
+
+        send_message_accept_terms = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id=".from_user.id",
+            text="Some text to accept terms and conditions... Do you accept?, send Yes or No",
+            position_x=1,
+            position_y=1,
+        )
+        SetState.objects.create(
+            bot=self.bot,
+            state="accept_terms",
+            position_x=1,
+            position_y=1,
+            previous_component=send_message_accept_terms,
+        )
+
+        Markup.objects.create(
+            parent_component=send_message_start,
+            markup_type=Markup.MarkupType.ReplyKeyboard,
+            buttons=[
+                [{"value": "Help", "next_component": send_message_help.id}],
+                [
+                    {
+                        "value": "Accept terms and conditions",
+                        "next_component": send_message_accept_terms.id,
+                    },
+                ],
+                [{"value": "Support", "next_component": send_support.id}],
+            ],
+        )
+
+        handle_support = OnMessage.objects.create(
+            bot=self.bot,
+            state="support",
+            position_x=1,
+            position_y=1,
+        )
+        send_message_to_admin = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id="693259126",
+            text="text send from user",
+            position_x=1,
+            position_y=1,
+            previous_component=handle_support,
+        )
+        send_message_to_user = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id=".from_user.id",
+            text="your message send to admin",
+            position_x=1,
+            position_y=1,
+            previous_component=send_message_to_admin,
+        )
+
+        handle_accept_terms = OnMessage.objects.create(
+            bot=self.bot,
+            state="accept_terms",
+            position_x=1,
+            position_y=1,
+        )
+        send_message_accept_terms_yes = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id=".from_user.id",
+            text="You accepted terms and conditions, send your phone number start with +98",
+            position_x=1,
+            position_y=1,
+        )
+
+        send_message_accept_terms_no = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id=".from_user.id",
+            text="You did not accept terms and conditions",
+            position_x=1,
+            position_y=1,
+        )
+
+        SwitchComponent.objects.create(
+            bot=self.bot,
+            position_x=1,
+            position_y=1,
+            previous_component=handle_accept_terms,
+            expression=".text",
+            values=["Yes", "No"],
+            next_components=[
+                send_message_accept_terms_yes.id,
+                send_message_accept_terms_no.id,
+            ],
+        )
+
+        phone_number_hanndle = OnMessage.objects.create(
+            bot=self.bot,
+            regex=True,
+            text="^\\+98\\d+$",
+            position_x=1,
+            position_y=1,
+        )
+        send_message_phone_number = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id=".from_user.id",
+            text="Is your phone number correct?",
+            position_x=1,
+            position_y=1,
+            previous_component=phone_number_hanndle,
+        )
+        phone_number_handle_yes_no = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id=".from_user.id",
+            text=".text",
+            position_x=1,
+            position_y=1,
+            previous_component=phone_number_hanndle,
+        )
+
+        handle_yes = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id=".from_user.id",
+            text="OK, thanks",
+            position_x=1,
+            position_y=1,
+        )
+
+        handle_no = SendMessage.objects.create(
+            bot=self.bot,
+            chat_id=".from_user.id",
+            text="OK, you lose..., send /start to start again",
             position_x=1,
             position_y=1,
         )
 
         Markup.objects.create(
-            parent_component=send_5,
-            markup_type=Markup.MarkupType.ReplyKeyboard,
+            parent_component=phone_number_handle_yes_no,
+            markup_type=Markup.MarkupType.InlineKeyboard,
             buttons=[
-                [{"value": "Button 1"}, {"value": "Button 2"}],
-                [{"value": "Button 3", "next_component": send_5.id}],
+                [{"value": "Yes", "next_component": handle_yes.id}],
+                [{"value": "No", "next_component": handle_no.id}],
             ],
-        )
-
-        on_message_regex = OnMessage.objects.create(
-            bot=self.bot,
-            text="Hello, .+",
-            position_x=1,
-            position_y=1,
-            regex=True,
-        )
-
-        send_6 = SendMessage.objects.create(
-            bot=self.bot,
-            chat_id=".from_user.id",
-            text="Hello, World! from regex",
-            position_x=1,
-            position_y=1,
-            previous_component=on_message_regex,
         )
 
     def test_create_bot(self):
